687190e66c8c26c9d745ce7f7ff1757318ad733f
danyllorobemar <danyllo.robemar@gmail.com>
2023-10-22 02:46:19 -0300

    Implementação da camada de domínio

---
+++ src/main/java/com/devdan/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devdan.domain.repository;
+
+import com.devdan.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
